8703bcdfd89e015468de1fc078c58a4e32e6914e
Jan Skowronski <crianonim@gmail.com>
2019-09-20 06:19:03 +0100

    Add systemctl service unit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const router = require("./router");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3171;
 
 http.createServer(router).listen(port, () => {
   console.log(`Server is listening on port:${port}`);
